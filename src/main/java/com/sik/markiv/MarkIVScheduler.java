1f88a711e6b388aa9cbd2b36d7195d193d4eb95f
Si K <simon.kusners@hermes-europe.co.uk>
2017-05-26 10:17:21 +0100

    Add banner

--- src/main/java/com/sik/markiv/MarkIVScheduler.java
+++ src/main/java/com/sik/markiv/MarkIVScheduler.java
@@ -13,15 +13,30 @@ import com.sik.markiv.google.calendar.MarkIVCalendarFeed;
 public class MarkIVScheduler {
 
 	private static final Logger LOG = Logger.getLogger(MarkIVScheduler.class);
-	
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(MarkIVScheduler.class);
-    }
-    
-    @Bean
+
+	public static void main(String[] args) throws Exception {
+
+		LOG.info("       ```````   ```````                          `..````               ````````````     ```````    \n"
+				+ "      -:/oooo:- -:+oooo:-                        `::+ooo:-             `::oooo/+ooo:-   -:oooo/-`   \n"
+				+ "      -/shhhh/-`:+hhhhh:-  .----------.`.---------:/hhhy--.----.       `/+hhhh-shhh:-  -/shhho-.    \n"
+				+ "     -/sdddddo.-/hddddd:- ./yhhhhhhhhy/:/ohhhyyhhhooddd/.:yhhy+/.      -/ydddo`sddd-.`:+hddd+..     \n"
+				+ "    .+smmdymmy.:dmmdmmm/- -+++++/+hmmm/:/ymmmyoooo-hmmh.odmms:-.      `+ommmm-.ymmm:-:odmmd/.`      \n"
+				+ "   `+omNN++NNd-hNNosNNN/-/shhhhhyymNNd.-+mNNh..`.:oNNNdmNNd+/--       /odNNNy.-hNNN:-smNNd-.`       \n"
+				+ "   +sdNNh./NNNhNNs.sNNN/-oNNNhyyydNNN+-:yNNN/-` :odNNmdddNNNN+/`     `osNNNN:-:hNNN:sNNNh--`        \n"
+				+ "  /shddh--/dddddo.-sddd/:dddo..:/dddh-:+dddy-- `symddo.--hddh-:      /sdddds-:+hdddhdddo--          \n"
+				+ " -yhNNN+-:+mNNNy--/hNNN/+NNNy+oshNNNo-/hNNN/-` :ydNNm-:/sNNNs-.     `yhNNNN::-odNNNNNm+-.           \n"
+				+ " yhNNNy--:sdNNh--.shNNN//mNNNNmsmNNm::omNNd-:  shmNNs-/odNNm::`     /hmNNNy---sdNNNNd:-.            \n"
+				+ " yysoo++:-yyso//``yyso++/+o+++/:++++/+osoo++/  sysoo++:ssso+++`     :hyso+++:.ysso++/:`             \n"
+				+ "  ``````   ````    ````` `````` ````` ``````    `````   `````         ``````   ``````               \n"
+				+ "                                                                                                    ");
+
+		SpringApplication.run(MarkIVScheduler.class);
+	}
+
+	@Bean
 	MarkIVCalendarFeed getFeed() {
 		LOG.info("Initialising Calendar feed...");
 		return new MarkIVCalendarFeed();
 	}
-    
+
 }
