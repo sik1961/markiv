76c51ad83f15d530e0b50de7f2c9533026e23cdb
Si K <sik@apples-Air.connect>
2019-12-07 21:55:57 +0000

    stop on first failure

--- src/main/java/com/sik/markiv/ftp/MarkIVUploader.java
+++ src/main/java/com/sik/markiv/ftp/MarkIVUploader.java
@@ -1,4 +1,5 @@
 package com.sik.markiv.ftp;
+
 /**
  * @author sik
  */
@@ -28,20 +29,18 @@ public class MarkIVUploader {
 		this.props = props;
 	}
 
-	public boolean upload(final String project, final List<String> files)
-			throws MarkIVException {
+	public boolean upload(final String project, final List<String> files) throws MarkIVException {
 
 		boolean uploadSuccessful = true;
 		final FTPClient ftp = new FTPClient();
 		try {
-			//ftp.setPassiveLocalIPAddress(InetAddress.getLocalHost());
+			// ftp.setPassiveLocalIPAddress(InetAddress.getLocalHost());
 			FileInputStream fis = null;
-		
+
 			LOG.debug("Connecting to: " + this.props.getProperty("FtpServer"));
 			ftp.connect(this.props.getProperty("FtpServer"));
 			LOG.debug("Logging in with id: " + this.props.getProperty("FtpId"));
-			boolean loginSucceeded = ftp.login(this.props.getProperty("FtpId"),
-					this.props.getProperty("FtpPw"));
+			boolean loginSucceeded = ftp.login(this.props.getProperty("FtpId"), this.props.getProperty("FtpPw"));
 			if (loginSucceeded) {
 				LOG.info("Login succeeded");
 			} else {
@@ -49,86 +48,79 @@ public class MarkIVUploader {
 			}
 
 			for (final String file : files) {
-				final String localFile = String.format("%s/%s",
-						this.props.getProperty("LocalWebRoot"), file);
+				final String localFile = String.format("%s/%s", this.props.getProperty("LocalWebRoot"), file);
 				fis = new FileInputStream(localFile);
-				final String remoteFile = String.format("%s/%s",
-						this.props.getProperty("RemoteWebRoot"), file);
+				final String remoteFile = String.format("%s/%s", this.props.getProperty("RemoteWebRoot"), file);
 
-				LOG.info(String.format("Uploading %s -> %s", localFile,
-						remoteFile));
+				LOG.info(String.format("Uploading %s -> %s", localFile, remoteFile));
 				boolean storeSucceeded = ftp.storeFile(remoteFile, fis);
 				if (!storeSucceeded) {
 					LOG.error("Upload failed - with code: " + ftp.getReplyString());
 					uploadSuccessful = false;
+					break;
 				}
 				fis.close();
 			}
 
-			Properties fileProps = pu.readProperties(props
-					.getProperty("UploadFilesList"));
-			boolean fileAdded = false;
-			for (String dir : props.getProperty("UploadDirs").split(",")) {
+			if (uploadSuccessful) {
+				Properties fileProps = pu.readProperties(props.getProperty("UploadFilesList"));
+				boolean fileAdded = false;
+				for (String dir : props.getProperty("UploadDirs").split(",")) {
 
-				File folder = new File(props.getProperty("LocalWebRoot") + dir);
-				List<String> localFiles = (getFileNames(Arrays.asList(folder
-						.listFiles())));
-				String remoteCsv = fileProps.getProperty(dir.replace("/", "."));
-				List<String> remoteFiles = null;
-				if (remoteCsv != null) {
-					remoteFiles = Arrays.asList(remoteCsv.split(","));
-				} else {
-					remoteFiles = new ArrayList<String>();
-				}
-				String newFiles = "";
-				for (String file : localFiles) {
-					if (!remoteFiles.contains(file)) {
-						final String localFile = String.format("%s/%s/%s",
-								this.props.getProperty("LocalWebRoot"), 
-								dir,
-								file);
-						fis = new FileInputStream(localFile);
-						final String remoteFile = String.format("%s/%s/%s",
-								this.props.getProperty("RemoteWebRoot"), 
-								dir,
-								file);
-
-						LOG.info(String.format("Uploading %s -> %s", localFile,
-								remoteFile));
-						boolean storeSucceeded = ftp.storeFile(remoteFile, fis);
-						if (!storeSucceeded) {
-							LOG.error("Upload failed - with code: " + ftp.getReplyString());
-							uploadSuccessful = false;
-						} 
-						fis.close();
-						newFiles = newFiles + "," + file;
-						fileAdded = true;
+					File folder = new File(props.getProperty("LocalWebRoot") + dir);
+					List<String> localFiles = (getFileNames(Arrays.asList(folder.listFiles())));
+					String remoteCsv = fileProps.getProperty(dir.replace("/", "."));
+					List<String> remoteFiles = null;
+					if (remoteCsv != null) {
+						remoteFiles = Arrays.asList(remoteCsv.split(","));
+					} else {
+						remoteFiles = new ArrayList<String>();
 					}
+					String newFiles = "";
+					for (String file : localFiles) {
+						if (!remoteFiles.contains(file)) {
+							final String localFile = String.format("%s/%s/%s", this.props.getProperty("LocalWebRoot"),
+									dir, file);
+							fis = new FileInputStream(localFile);
+							final String remoteFile = String.format("%s/%s/%s", this.props.getProperty("RemoteWebRoot"),
+									dir, file);
+
+							LOG.info(String.format("Uploading %s -> %s", localFile, remoteFile));
+							boolean storeSucceeded = ftp.storeFile(remoteFile, fis);
+							if (!storeSucceeded) {
+								LOG.error("Upload failed - with code: " + ftp.getReplyString());
+								uploadSuccessful = false;
+								break;
+							}
+							fis.close();
+							newFiles = newFiles + "," + file;
+							fileAdded = true;
+						}
+					}
+					if (fileAdded) {
+						String propValue = fileProps.getProperty(dir.replace("/", ".")) + newFiles;
+						String propKey = dir.replace("/", ".");
+						LOG.info("Setting property: " + propKey + " to: " + propValue);
+						fileProps.setProperty(propKey, propValue);
+					}
+
 				}
 				if (fileAdded) {
-					String propValue = fileProps.getProperty(dir.replace("/", ".")) + newFiles;
-					String propKey = dir.replace("/", ".");
-					LOG.info("Setting property: " + propKey + " to: "
-							+ propValue);
-					fileProps.setProperty(propKey, propValue);
+					LOG.info("Writing properties: " + props.getProperty("UploadFilesList"));
+					pu.writeProperties(fileProps, props.getProperty("UploadFilesList"));
 				}
-			}
-			if (fileAdded) {
-				LOG.info("Writing properties: "	+ props.getProperty("UploadFilesList"));
-				pu.writeProperties(fileProps, props.getProperty("UploadFilesList"));
 			}
 
 			LOG.debug("Logging out...");
 			ftp.logout();
-			ftp.disconnect();
+			//ftp.disconnect();
 			if (uploadSuccessful) {
 				LOG.info("Upload completed successfully");
 			} else {
 				LOG.info("Upload failed");
 			}
 		} catch (final org.apache.commons.net.ftp.FTPConnectionClosedException cc) {
-			throw new MarkIVException(
-					"Upload not completed - Connection was closed unexpectedly");
+			throw new MarkIVException("Upload not completed - Connection was closed unexpectedly");
 		} catch (final IOException e) {
 			throw new MarkIVException("I/O error occurred:" + e.getMessage());
 		} catch (final Exception e) {
